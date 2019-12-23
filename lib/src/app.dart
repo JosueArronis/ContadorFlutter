a60689d0128934301e64c488f9ac9ac702e90f14
Josue Arronis <cruzjos94@gmail.com>
2019-12-23 15:02:00 -0600

    App contador

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:contador/src/pages/contador_page.dart';
+// import 'package:contador/src/pages/home_page.dart';
 
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Center(
-        child:  Text('Hola Mundo !!'),
+        child: ContadorPage(),
       )
     );
   }
